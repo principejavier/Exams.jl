c97bd230756eaa1daf34f75c3b607ba6d1cfa721
principe <principe@cimne.upc.edu>
2023-09-11 10:17:56 +0200

    Online exam working wo clean management of figs

--- src/Exams.jl
+++ src/Exams.jl
@@ -39,6 +39,10 @@ export StandardTemplate
 export StandardHeadings
 export PrintedExam
 export PrintedQuestion
+export OnlineExam
+export OnlineQuestion
+export OnlineHeadings
+
 export add_problem!
 export add_pagebreak!
 export add_vspace!
@@ -52,8 +56,8 @@ export figure
 export Format
 export FloatingFigure
 export WrappedFigure
+export InlinedFigure
 export NoFigure
-export Unformatted
 export ALL2ALL
 export ONE2ONE
 export SPA
@@ -84,14 +88,13 @@ export PropsSI
 const MoodleTemplate="""
 \\documentclass[11pt]{article}
 \\usepackage{amssymb,amsmath}
-\\usepackage[utf8]{inputenc}  % handle accents in pdf, using it causes errors in xml
-\\usepackage{unicode-math}    % Correctly handles unicode characters (degree) in pdf, only with lualatex
 \\usepackage{moodle}          % generate xml
 \\usepackage[catalan]{babel} % hyphentation
 \\usepackage{graphics}
 \\usepackage{pgf}
 \\usepackage{tikz}
 \\usepackage{hyperref}
+\\graphicspath{ {{{GraphicsPath}}} }
 \\begin{document}
 
 """
@@ -185,6 +188,11 @@ StandardHeadings[SPA]["ProblemName"]="Problema"
 StandardHeadings[CAT]["ProblemName"]="Problema"
 StandardHeadings[ENG]["ProblemName"]="Problem"
 
+OnlineHeadings = Dict{String,Dict{String, String}}()
+OnlineHeadings[SPA]=Dict{String,String}()
+OnlineHeadings[CAT]=Dict{String,String}()
+OnlineHeadings[ENG]=Dict{String,String}()
+
 function add_defaults!(headings)
     for (key,val) in StandardHeadings
       if !haskey(headings,key)
@@ -193,11 +201,14 @@ function add_defaults!(headings)
     end
 end
 
-abstract type FormatFigure end
-struct FloatingFigure <: FormatFigure
+abstract type LatexFigure end
+struct FloatingFigure <: LatexFigure
     width::Float64
 end
-struct WrappedFigure <: FormatFigure
+struct InlinedFigure <: LatexFigure
+    width::Float64
+end
+struct WrappedFigure <: LatexFigure
     width::Float64
     width_fig::Float64
     #vshift::Any
@@ -209,14 +220,13 @@ struct WrappedFigure <: FormatFigure
         new(w,width_fig,v,lines,hang)
     end
 end
-struct NoFigure<:FormatFigure end
+struct NoFigure<:LatexFigure end
 
 abstract type ArgCombination end
 struct ALL2ALL <: ArgCombination end
 struct ONE2ONE <: ArgCombination end
 
 abstract type QuestionFormat end
-struct Unformatted <: QuestionFormat end
 struct PrintedQuestion <: QuestionFormat
     num_rows    :: Vector{Int}
     num_options :: Vector{Int}
@@ -236,13 +246,55 @@ function PrintedQuestion(
     right = Vector{Vector{Int}}(undef,default_max_questions)
     PrintedQuestion(num_rows,num_options,width,int_params,right)
 end
-
 struct OnlineQuestion <: QuestionFormat
     num_options :: Vector{Int}
     int_params  :: Vector{Int}
 end
+function OnlineQuestion(num_options=[5 for i in 1:default_max_questions])
+    max_questions = length(num_options)
+    @assert num_options == [5 for i=1:max_questions] "The number of options is currently hard-coded to 5"
+    int_params = Vector{Int}(undef,2)
+    OnlineQuestion(num_options,int_params)
+end
 
 abstract type Exam end
+function add_arguments!(exam::Exam,v::Vector{T} where T)
+    @assert false
+end
+function add_function!(exam::Exam,f::Function)
+    @assert false
+end
+function add_figure!(exam::Exam,fig::LatexFigure)
+    @assert false
+end
+function add_new_problem!(exam::Exam)
+    @assert false
+end
+
+function add_problem!(exam::Exam,fig::LatexFigure,c::Type{<:ArgCombination},f::Function,args...)
+    add_new_problem!(exam)
+    add_function!(exam,f)
+    add_figure!(exam,fig)
+
+    for v in args
+        @assert isa(v,Vector) "To add a function provide its arguments as vectors (components of which are used to generate combinations)"
+    end
+    if length(args)==0 # function without args
+        add_arguments!(exam,[() for i in 1:exam.num_permutations])
+    else
+        if c===ALL2ALL
+            @assert length(Iterators.product(args...))>=exam.num_permutations "Not enough arguments for problem $f"
+            add_arguments!(exam,collect(Iterators.take(Iterators.product(args...),exam.num_permutations)))
+        elseif c===ONE2ONE
+            # @assert length(Iterators.product(args...))>=exam.num_permutations^2 "Not enough arguments for problem $f"
+            # push!(exam.arguments,takediag(Iterators.product(args...),exam.num_permutations))
+            for (i,a) in enumerate(args)
+                @assert length(a)>= get_num_permutations(exam) "Argument $i is not of the required size"
+            end
+            add_arguments!(exam,collect(zip(args...)))
+        end
+    end
+end
 
 """
     struct PrintedExam <: Exam
@@ -277,7 +329,7 @@ struct PrintedExam <: Exam
     languages::Vector{String}
     headings::Dict{String,Dict{String, String}}
     template::String
-    figures::Vector{FormatFigure}
+    figures::Vector{LatexFigure}
     functions::Vector{Function}
     arguments::Vector{Vector{Tuple}}
     vspace::Vector{Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units}}
@@ -286,8 +338,38 @@ struct PrintedExam <: Exam
     function PrintedExam(num_permutations;languages=[ENG],headings=StandardHeadings,name="exam",template=StandardTemplate,format=PrintedQuestion())
         define_correct_answers!(format,name,num_permutations)
         add_defaults!(headings)
-        new(name,num_permutations,languages,headings,template,Vector{FormatFigure}(undef,0),Vector{Function}(undef,0),Vector{Vector{Tuple}}(undef,0),Vector{Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units}}(undef,0),Vector{Bool}(undef,0),format)
+        new(name,num_permutations,languages,headings,template,Vector{LatexFigure}(undef,0),Vector{Function}(undef,0),Vector{Vector{Tuple}}(undef,0),Vector{Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units}}(undef,0),Vector{Bool}(undef,0),format)
     end
+end
+
+function add_pagebreak!(exam::PrintedExam)
+    exam.pagebreak[end]=true
+end
+
+function add_vspace!(exam::PrintedExam,s::Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units})
+    exam.vspace[end]=s
+end
+
+function add_arguments!(exam::PrintedExam,v::Vector{T} where T)
+    push!(exam.arguments,v)
+end
+
+function add_function!(exam::PrintedExam,f::Function)
+    push!(exam.functions,f)
+end
+
+function add_figure!(exam::PrintedExam,fig::LatexFigure)
+    push!(exam.figures,fig)
+end
+function add_new_problem!(exam::PrintedExam)
+    push!(exam.vspace,0.0cm)
+    push!(exam.pagebreak,false)
+end
+function get_languages(exam::PrintedExam)
+    exam.languages
+end
+function get_num_permutations(exam::PrintedExam)
+    exam.num_permutations
 end
 
 struct OnlineExam <: Exam
@@ -296,59 +378,37 @@ struct OnlineExam <: Exam
     languages::Vector{String}
     headings::Dict{String,Dict{String, String}}
     template::String
-    figures::Vector{FormatFigure}
+    figures::Vector{LatexFigure}
     functions::Vector{Function}
     arguments::Vector{Vector{Tuple}}
     format::QuestionFormat
-    function OnlineExam(num_permutations;languages=[ENG],headings=OnlineHeadings,name="exam",template=StandardTemplate,format=Format())
-        # Some checks
-        max_questions = length(format.rows)
-        @assert length(format.num_options) == max_questions
-        @assert length(format.width) == max_questions
-        @assert format.options == [5 for i=1:max_questions] "The number of options is currently hard-coded to 5"
-        right = Matrix{Int64}(undef,max_questions, num_permutations)
-        define_correct_anwsers!(right,name,max_questions,num_permutations)
-        form=PrintedQuestion(format.num_options,format.num_rows,Vector{Int}(undef,2),right,format.width)
-        add_defaults!(headings)
-        new(name,num_permutations,max_questions,languages,headings,template,Vector{FormatFigure}(undef,0),Vector{Function}(undef,0),Vector{Vector{Tuple}}(undef,0),Vector{Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units}}(undef,0),Vector{Bool}(undef,0),form)
+    function OnlineExam(num_permutations;languages=[ENG],headings=MoodleHeadings,name="exam",template=MoodleTemplate,format=OnlineQuestion())
+        new(name,num_permutations,languages,headings,template,Vector{LatexFigure}(undef,0),Vector{Function}(undef,0),Vector{Vector{Tuple}}(undef,0),format)
     end
 end
 
+function add_arguments!(exam::OnlineExam,v::Vector{T} where T)
+    push!(exam.arguments,v)
+end
 
-
-function add_problem!(exam::PrintedExam,fmt::FormatFigure,c::Type{<:ArgCombination},f::Function,args...)
+function add_function!(exam::OnlineExam,f::Function)
     push!(exam.functions,f)
-    push!(exam.figures,fmt)
-    push!(exam.vspace,0.0cm)
-    push!(exam.pagebreak,false)
-    for v in args
-        @assert isa(v,Vector) "To add a function provide its arguments as vectors (used to generate combinations)"
-    end
-    if length(args)==0 # function without args
-        push!(exam.arguments,[() for i in 1:exam.num_permutations])
-    else
-        if c===ALL2ALL
-            @assert length(Iterators.product(args...))>=exam.num_permutations "Not enough arguments for problem $f"
-            push!(exam.arguments,collect(Iterators.take(Iterators.product(args...),exam.num_permutations)))
-        elseif c===ONE2ONE
-            # @assert length(Iterators.product(args...))>=exam.num_permutations^2 "Not enough arguments for problem $f"
-            # push!(exam.arguments,takediag(Iterators.product(args...),exam.num_permutations))
-            for (i,a) in enumerate(args)
-                @assert length(a)>=exam.num_permutations "Argument $i is not of the required size"
-            end
-            push!(exam.arguments,collect(zip(args...)))
-        end
-    end
 end
 
-function add_pagebreak!(exam::PrintedExam)
-    exam.pagebreak[end]=true
+function add_figure!(exam::OnlineExam,fig::LatexFigure)
+    @assert isa(fig,InlinedFigure) "Only InlinedFigure is valid for OnlineExam"
+    push!(exam.figures,fig)
 end
-function add_vspace!(exam::PrintedExam,s::Quantity{T, Unitful.𝐋, U} where {T<:Real,U<:Unitful.Units})
-    exam.vspace[end]=s
+function add_new_problem!(exam::OnlineExam)
+end
+function get_languages(exam::OnlineExam)
+    exam.languages
+end
+function get_num_permutations(exam::OnlineExam)
+    exam.num_permutations
 end
 
-function generate_pdf_files(exam::PrintedExam)
+function generate_pdf_files(exam::Exam)
     generate_tex_files(exam)
     compile_tex_files(exam)
 end
@@ -390,12 +450,46 @@ function generate_tex_files(exam::PrintedExam)
 
 end
 
-function compile_tex_files(exam::PrintedExam)
+function generate_tex_files(exam::OnlineExam)
 
-    for lang in exam.languages
-        for i = 1:exam.num_permutations
+    num_prob = length(exam.functions)
+
+    for lang in get_languages(exam)
+        for i = 1:get_num_permutations(exam)
+            exam.format.int_params[1] = i
+            exam.format.int_params[2] = 0
             filename = exam.name*"_"*lang*"_$i"
-            pdflatex = `lualatex $(filename)`
+            io_tex = open(filename*".tex","w");
+            render(io_tex,replace(exam.template,"\\end{document}"=>""),exam.headings[lang])
+            # Loop over problems
+            write(io_tex,begin_quiz(lang,exam.name))
+            last_page=""
+            for k = 1:num_prob
+                write(io_tex,begin_cloze(exam.name))
+                problem_slices=exam.functions[k](exam.arguments[k][i]...,lang,exam.format)
+                last_page=last_page*format_figure!(exam.figures[k],problem_slices)
+                problem=prod(problem_slices)
+                write(io_tex, problem);
+                write(io_tex, end_cloze())
+            end
+            write(io_tex, end_quiz())
+            write(io_tex, last_page)
+            write(io_tex, latex_tail);
+            close(io_tex);
+        end
+    end
+
+    return nothing
+
+end
+
+function compile_tex_files(exam::Exam)
+
+    for lang in get_languages(exam)
+        for i = 1:get_num_permutations(exam)
+            filename = exam.name*"_"*lang*"_$i"
+            # pdflatex = `lualatex $(filename)`
+            pdflatex = `pdflatex $(filename)`
             run(pdflatex)
             run(pdflatex)
             rm(filename*".aux")
@@ -444,7 +538,6 @@ end
 
 function figure(name, label="", caption="")
     figure = """
-    \\centering
     \\includegraphics[width]{$name}
     """
     (caption > "")  && ( figure = figure*"\\caption{$caption} \n" )
@@ -485,7 +578,23 @@ function format_figure!(format::FloatingFigure,str::Vector{String})
     w=format.width
     for (i,s) in enumerate(str)
         if occursin("includegraphics",s) 
-            str[i]="\n\n\\begin{figure}[h!]\n"*replace(s,"width"=>"width=$w\\textwidth")*"\\end{figure}\n\n"
+            str[i]="\n\n\\begin{figure}[h!]\n\\begin{center}\n"*replace(s,"width"=>"width=$w\\textwidth")*"\\end{center}\n\\end{figure}\n\n"
+            break
+        end
+    end
+    return "\n"
+end
+
+function format_figure!(format::InlinedFigure,str::Vector{String})
+
+    @assert length(str)>0 "Empty string vector in format_figure!(FloatingFigure,...)"
+    w=format.width
+    for (i,s) in enumerate(str)
+        if occursin("includegraphics",s) 
+            # t=replace(s,"\\centering"=>"\n")
+            t=replace(s,r"\\caption{.*}"=>"\n")
+            t=replace(t,r"\\label{.*}"=>"\n")
+            str[i]="\n\n\\begin{center}\n"*replace(t,"width"=>"width=$w\\textwidth")*"\\end{center}\n\n"
             break
         end
     end
@@ -497,7 +606,7 @@ function format_figure!(format::NoFigure,str::Vector{String})
     return "\n"
 end
 
-function begin_quiz(name)
+function begin_quiz(language,name)
     str = "\\begin{quiz}{Probs/$language/$name}\n"
     return str
 end
@@ -505,6 +614,14 @@ function begin_cloze(name)
     str = "\\begin{cloze}{$name}\n"
     return str
 end
+
+function end_cloze()
+    "\\end{cloze}\n\n"
+end
+function end_quiz()
+    "\\end{quiz}\n"
+end
+
 
 function begin_problem(prob)
     str = "\n\\noindent {\\bfseries {{{ProblemName}}} $prob} \n\n"
@@ -595,10 +712,7 @@ function rand2()
     end
 end
 
-function question(::Type{Unformatted}, msg, var, unitname=""; factor1=rand2(), factor2=1.2 + 0.2 * rand(), factor3=0.6 + 0.2 * rand(), factor4=1.6 + 0.2 * rand(),num_digits=4)
-
-    # Next lines are in common with other methods, could be wrapped into
-    # another function
+function get_value(var,unitname,num_digits)
     if unitname > "" # write it as given and pick the value parsing it
         latex_unit = unit2latex(unitname)
         out_unit   = eval(Meta.parse(unitname))
@@ -607,32 +721,20 @@ function question(::Type{Unformatted}, msg, var, unitname=""; factor1=rand2(), f
         unitname   = string(out_unit)
         latex_unit = unit2latex(unitname)
     end
-   
     val = ustrip(Float64, out_unit, var)
     val = round(val, sigdigits=num_digits)
-    res = val2latex(val)
+    return val, latex_unit
+end
 
+function question(::Nothing, msg, var, unitname=""; factor1=rand2(), factor2=1.2 + 0.2 * rand(), factor3=0.6 + 0.2 * rand(), factor4=1.6 + 0.2 * rand(), num_digits=4)
+    val, latex_unit = get_value(var,unitname,num_digits)
+    res = val2latex(val)
     msg = replace(msg, "unitname" => latex_unit)
     println(msg*" Result: $res")
-
 end
 
 function question(format::PrintedQuestion, msg, var, unitname=""; factor1=rand2(), factor2=1.2 + 0.2 * rand(), factor3=0.6 + 0.2 * rand(), factor4=1.6 + 0.2 * rand(),num_digits=4)
-
-    # Unit
-    if unitname > "" # write it as given and pick the value parsing it
-        latex_unit = unit2latex(unitname)
-        out_unit   = eval(Meta.parse(unitname))
-    else # pick the unit from the variable
-        out_unit   = unit(var)
-        unitname   = string(out_unit)
-        latex_unit = unit2latex(unitname)
-    end
-   
-    val = ustrip(Float64, out_unit, var)
-#    tol = round(tolerance * abs(val), sigdigits=2)
-    val = round(val, sigdigits=num_digits)
-
+    val, latex_unit = get_value(var,unitname,num_digits)
     msg = replace(msg, "unitname" => latex_unit)
 
     val1 = round(factor1 * val, sigdigits=num_digits); # 0.0 
@@ -644,7 +746,7 @@ function question(format::PrintedQuestion, msg, var, unitname=""; factor1=rand2(
     num_question = format.int_params[2]
     pos = format.right[format.int_params[2]][format.int_params[1]]
     res = Array{String}(undef,5);
-    #res = zeros(5);
+
     res[mod(pos, 5) + 1] = val2latex(val1)
     res[mod(pos + 1, 5) + 1] = val2latex(val2)
     res[mod(pos + 2, 5) + 1] = val2latex(val3)
@@ -652,21 +754,6 @@ function question(format::PrintedQuestion, msg, var, unitname=""; factor1=rand2(
     res[mod(pos + 4, 5) + 1] = val2latex(val)
 
     str=print_answers(format.width[num_question],format.num_rows[num_question],num_question,msg,res)
-
-    # str = """\\textbf{$num_question} $msg \\\\
-    # \\begin{tabular}{*{3}{p{2.5cm}}}
-    # a) $(res[1]) & b) $(res[2]) & c) $(res[3]) \\\\ d) $(res[4]) & e) $(res[5]) & f) \\rule[0pt]{1cm}{0.75pt} \\\\
-    # \\end{tabular}
-
-    # """
-
-    # str = """\n
-    # \\textbf{$num_question} $msg \\\\
-    # \\begin{tabular}{*{5}{p{2.cm}}}
-    # a) \$$(res[1])\$ & b) \$$(res[2])\$ & c) \$$(res[3])\$ & d) \$$(res[4])\$ & e) \$$(res[5])\$ \\\\
-    # \\end{tabular}
-
-    # """
     return str
 
 end
@@ -712,5 +799,31 @@ function print_answers(w,k,n,msg,res)
     end
 end
 
+function question(format::OnlineQuestion, msg, var, unitname=""; factor1=rand2(), factor2=1.2 + 0.2 * rand(), factor3=0.6 + 0.2 * rand(), factor4=1.6 + 0.2 * rand(),num_digits=4)
+
+    val, latex_unit = get_value(var,unitname,num_digits)
+    msg = replace(msg, "unitname" => latex_unit)
+    format.int_params[2]=format.int_params[2]+1
+    num_question = format.int_params[2]
+
+    val1 = round(factor1*val, sigdigits=num_digits);
+    val2 = round(factor2*val, sigdigits=num_digits);
+    val3 = round(factor3*val, sigdigits=num_digits);
+    val4 = round(factor4*val, sigdigits=num_digits);
+    
+    str="""
+    \\begin{multi}$num_question. $msg \\\\ 
+    \\item* $val
+    \\item PENALTY$val1
+    \\item PENALTY$val2
+    \\item PENALTY$val3
+    \\item PENALTY$val4
+    \\item No contesta
+    \\end{multi}
+
+    """
+
+    return str
+end
 
 end
---
+++ test/example_moodle.jl
@@ -0,0 +1,22 @@
+using Exams
+include("problems.jl")
+
+function generate_quiz(nump)
+
+    headings=OnlineHeadings
+
+    headings[SPA]["GraphicsPath"] = "{../assets/}"
+    headings[CAT]["GraphicsPath"] = "{../assets/}"
+    headings[ENG]["GraphicsPath"] = "{../assets/}"
+
+    quiz = OnlineExam(nump,languages=[SPA],headings=headings,name="MoodleExamExample")
+    mass=[1kg,2kg];
+    h0=[1m,2m];
+    v0=[1m/s,2m/s];
+    add_problem!(quiz,InlinedFigure(0.4),ONE2ONE,free_fall,mass,h0,v0)
+
+    generate_tex_files(quiz)
+    compile_tex_files(quiz)
+    return nothing
+
+end
